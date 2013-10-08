1c3cfad0e47a5e8d62861bcd9b4f0087525f9f9c
YangTseChung <youngce0918@gmail.com>
2013-10-08 11:16:48 +0800

    AutoMapper.AutoMapperMappingException

---
+++ AutoMapperTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper_BookStore_Demo;
+
+namespace AutoMapperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mapper = AutoMapper.Mapper.Map<AddressDto, Address>(new AddressDto());
+           
+        }
+    }
+}
