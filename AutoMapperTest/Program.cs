c01c8e126061a9f33dc061fd287324f1e180c2c0
YangTseChung <youngce0918@gmail.com>
2013-10-10 14:49:23 +0800

    try BookStore and BookStoreDto mapper

--- AutoMapperTest/Program.cs
+++ AutoMapperTest/Program.cs
@@ -1,24 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using AutoMapper;
 using AutoMapper_BookStore_Demo;
 
 namespace AutoMapperTest
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Mapper.CreateMap<AddressDto, Address>();
-            var mapper = AutoMapper.Mapper.Map<AddressDto, Address>(new AddressDto
-                                                                    {
-                                                                        Country = "Taiwan",
-                                                                        Street = "a street"
+            var createAddressMap=Mapper.CreateMap<AddressDto, Address>();
 
-                                                                    });
-           
+            var address = Mapper.Map<AddressDto, Address>(new AddressDto
+                                                              {
+                                                                  Country = "Taiwan",
+                                                                  Street = "a street"
+                                                              });
+            
+
+            var bookStoreDto = new BookStoreDto
+                      {
+                          Name = "My Store",
+                          Address = new AddressDto {City = "Beijing"},
+                          Books =
+                              new List<BookDto>
+                              {
+                                  new BookDto
+                                  {
+                                      Title = "RESTful Web Service"
+                                  },
+                                  new BookDto {Title = "Ruby for Rails"},
+                              }
+                      };
+            IMappingExpression<BookDto, Book> expression = Mapper.CreateMap<BookDto, Book>(); 
+            
+            var createBookStoreMap= Mapper.CreateMap<BookStoreDto, BookStore>();
+            var bookStore = Mapper.Map<BookStoreDto, BookStore>(bookStoreDto);
+
+
         }
     }
 }
